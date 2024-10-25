d887b5cf3a02d86731c0fb07286a2818af61fa49
Chokpaisan Sripraiwan <chokpaisan.work@gmail.com>
2024-10-25 23:10:12 +0700

    [Update] icon

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -2,20 +2,18 @@ import './globals.css'
 import { Inter } from 'next/font/google'
 import { Suspense } from 'react'
 import NavBars, { NavigationEvents } from '../components/NavBars'
-import logoIcon from './coin.ico'
 import Providers from '@/components/Providers'
 const inter = Inter({ subsets: ['latin'] })
 export const metadata = {
   title: 'Expense WebApplication',
-  description: 'Survey your statement',
-  icons: {
-    icon: '/coin.ico',
-  }
+  description: 'Survey your statement'
 }
+
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <link rel="icon" type="image/x-icon" href="/coin.ico" />
       <body className={inter.className}>
         <Providers>
           {/* <NavBars /> */}
